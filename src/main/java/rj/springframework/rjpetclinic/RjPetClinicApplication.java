99002d8c26f5d97e5de328cdd00b7dd6113b9b35
ranjitjena1986 <ranjitjena199@gmail.com>
2020-05-15 20:14:56 +0530

    Initial project added

---
+++ src/main/java/rj/springframework/rjpetclinic/RjPetClinicApplication.java
@@ -0,0 +1,13 @@
+package rj.springframework.rjpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RjPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RjPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rj/springframework/rjpetclinic/RjPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package rj.springframework.rjpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RjPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
